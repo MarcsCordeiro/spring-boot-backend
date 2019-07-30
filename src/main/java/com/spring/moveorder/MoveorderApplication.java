44298910a8a0edf2975c889a03f7ae3af38e4040
Marcos Vicente <vicentevieira300@gmail.com>
2019-07-30 01:14:46 -0300

    Projeto iniciado com REST funcionando

---
+++ src/main/java/com/spring/moveorder/MoveorderApplication.java
@@ -0,0 +1,13 @@
+package com.spring.moveorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveorderApplication.class, args);
+	}
+
+}
