eb34d1d4adf41f42ea34be97fe2beda022d75c37
Marcos Vicente <vicentevieira300@gmail.com>
2019-07-30 02:21:36 -0300

    Criada a classe model-Categotias testada com postman

--- src/main/java/com/spring/moveorder/resources/CategoriaResource.java
+++ src/main/java/com/spring/moveorder/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.spring.moveorder.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.moveorder.model.Categorias;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!!!";
+	public List<Categorias> listar() {
+		
+		Categorias cat1 = new Categorias(1, "Informática");
+		Categorias cat2 = new Categorias(2, "Escritório");
+		
+		List<Categorias> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
