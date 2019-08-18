01ded8f64f825ed839e4edf56b29d2be2d96f456
Marcos Vicente <vicentevieira300@gmail.com>
2019-08-18 17:51:10 -0300

    Pedido,Pagamento,EstadoPagamento implementados

--- src/main/java/com/spring/moveorder/MoveorderApplication.java
+++ src/main/java/com/spring/moveorder/MoveorderApplication.java
@@ -1,5 +1,6 @@
 	package com.spring.moveorder;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.spring.moveorder.model.Cidade;
 import com.spring.moveorder.model.Cliente;
 import com.spring.moveorder.model.Endereco;
 import com.spring.moveorder.model.Estado;
+import com.spring.moveorder.model.Pagamento;
+import com.spring.moveorder.model.PagamentoComBoleto;
+import com.spring.moveorder.model.PagamentoComCartao;
+import com.spring.moveorder.model.Pedido;
 import com.spring.moveorder.model.Produto;
+import com.spring.moveorder.model.enums.EstadoPagamento;
 import com.spring.moveorder.model.enums.TipoCliente;
 import com.spring.moveorder.repositories.CategoriaRepository;
 import com.spring.moveorder.repositories.CidadeRepository;
 import com.spring.moveorder.repositories.ClienteRepository;
 import com.spring.moveorder.repositories.EnderecoRepository;
 import com.spring.moveorder.repositories.EstadoRepository;
+import com.spring.moveorder.repositories.PagamentoRepository;
+import com.spring.moveorder.repositories.PedidoRepository;
 import com.spring.moveorder.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -35,6 +43,10 @@ public class MoveorderApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoveorderApplication.class, args);
@@ -83,7 +95,21 @@ public class MoveorderApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-	}
-
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/06/2019 10:35"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2018 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("25/10/2018 00:00"), null);
+		ped2.setPagamento(pagto2);
 	
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+	}
 }
---
+++ src/main/java/com/spring/moveorder/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.spring.moveorder.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.moveorder.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
