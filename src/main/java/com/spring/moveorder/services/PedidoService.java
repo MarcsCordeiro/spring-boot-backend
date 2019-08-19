fc72fa8799e7435d72981d3b55aa685700d96716
Marcos Vicente <vicentevieira300@gmail.com>
2019-08-19 09:41:30 -0300

    Endpoint Pedidos funcionando

---
+++ src/main/java/com/spring/moveorder/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.spring.moveorder.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.moveorder.model.Pedido;
+import com.spring.moveorder.repositories.PedidoRepository;
+import com.spring.moveorder.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
