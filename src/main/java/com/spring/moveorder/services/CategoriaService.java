c5214b14a2d2e643417ac71de79696d0c8553b2a
Marcos Vicente <vicentevieira300@gmail.com>
2019-08-21 11:10:22 -0300

    Deletando Categoria DELETE

--- src/main/java/com/spring/moveorder/services/CategoriaService.java
+++ src/main/java/com/spring/moveorder/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.spring.moveorder.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.spring.moveorder.model.Categoria;
 import com.spring.moveorder.repositories.CategoriaRepository;
+import com.spring.moveorder.services.exceptions.DataIntegrityException;
 import com.spring.moveorder.services.exceptions.ObjectNotFoundException;
 
 
@@ -32,5 +34,14 @@ public class CategoriaService{
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categotia que tenha produtos.");
+		}
+	}
 }
 //"Objeto não encontrado! Id: " + id + ", Tipo:" + Categoria.class.getName()
