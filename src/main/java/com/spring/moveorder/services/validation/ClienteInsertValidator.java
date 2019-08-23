37dec3ca704dd9911d2e45347f543a546c472d60
Marcos Vicente <vicentevieira300@gmail.com>
2019-08-23 01:07:51 -0300

    Altenticacao de email cliente para nao ter repeticao de email

--- src/main/java/com/spring/moveorder/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/spring/moveorder/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.spring.moveorder.dto.ClienteNewDTO;
+import com.spring.moveorder.model.Cliente;
 import com.spring.moveorder.model.enums.TipoCliente;
+import com.spring.moveorder.repositories.ClienteRepository;
 import com.spring.moveorder.resources.exception.FieldMessage;
 import com.spring.moveorder.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente."));
 		}
 		
 		for (FieldMessage e : list) {
