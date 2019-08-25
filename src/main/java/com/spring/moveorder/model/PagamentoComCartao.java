ea403603c3f1cb3e67544926adbe95492c199ba4
Marcos Vicente <vicentevieira300@gmail.com>
2019-08-25 02:05:35 -0300

    Inserindo Pedido

--- src/main/java/com/spring/moveorder/model/PagamentoComCartao.java
+++ src/main/java/com/spring/moveorder/model/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.spring.moveorder.model;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.spring.moveorder.model.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
